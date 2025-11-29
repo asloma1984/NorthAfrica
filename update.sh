8d88ce83d19f08e0c335bbcf11b2b3a2e08f2fd7
abdul <59056864+asloma1984@users.noreply.github.com>
2025-11-29 16:53:45 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # System Request : Debian 9+/Ubuntu 18.04+/20+
-# Develovers » Tøxic savage࿐
-# Email      » johntezali56@gmail.com
-# telegram   » https://t.me/ToxicSavage
-# whatsapp   » wa.me/+254716637803
+# Develovers  » Tøxic savage࿐
+# Email       » johntezali56@gmail.com
+# telegram    » https://t.me/ToxicSavage
+# whatsapp    » wa.me/+254716637803
+#
+# Personal TG » @Abdulsalam403
+# Bot         » @NorthSSHAfrica5_bot
+# Channel     » https://t.me/northafrica9
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 clear
+
 fun_bar() {
     CMD[0]="$1"
     CMD[1]="$2"
     (
-        [[ -e $HOME/fim ]] && rm $HOME/fim
+        [[ -e $HOME/fim ]] && rm "$HOME/fim"
         ${CMD[0]} -y >/dev/null 2>&1
         ${CMD[1]} -y >/dev/null 2>&1
-        touch $HOME/fim
+        touch "$HOME/fim"
     ) >/dev/null 2>&1 &
     tput civis
-    echo -ne "\033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    echo -ne "\033[0;33mPlease wait, loading \033[1;37m- \033[0;33m["
     while true; do
         for ((i = 0; i < 18; i++)); do
             echo -ne "\033[0;32m#"
             sleep 0.1s
         done
-        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        [[ -e $HOME/fim ]] && rm "$HOME/fim" && break
         echo -e "\033[0;33m]"
         sleep 1s
         tput cuu1
         tput dl1
-        echo -ne "\033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+        echo -ne "\033[0;33mPlease wait, loading \033[1;37m- \033[0;33m["
     done
-    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK!\033[1;37m"
     tput cnorm
 }
+
 res1() {
     wget https://raw.githubusercontent.com/NorthAfrica/upload/main/menu/menu.zip
     unzip menu.zip
@@ -43,6 +49,7 @@ res1() {
     rm -rf menu.zip
     rm -rf update.sh
 }
+
 netfilter-persistent
 clear
 
@@ -51,7 +58,7 @@ echo -e "━━━━━━━━━━━━━━━━━━━━━━━�
 echo -e "\e[1;97;101m            » UPDATE SCRIPT «             \033[0m"
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 echo -e ""
-echo -e "\033[1;91mUpdate Script Service\033[1;37m"
+echo -e "\033[1;91mUpdate North Africa Script Service\033[1;37m"
 fun_bar 'res1'
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 echo -e ""
