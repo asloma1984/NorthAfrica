aabe4132a9a38c325a3a7043abc8dc05df52436a
abdul <59056864+asloma1984@users.noreply.github.com>
2025-11-29 19:15:14 +0000

    Update bbr.sh

--- files/bbr.sh
+++ files/bbr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# System Request : Debian 9+/Ubuntu 18.04+/20+
-# Develovers » Tøxic savage࿐
-# Email      » johntezali56@gmail.com
-# telegram   » https://t.me/ToxicSavage
-# whatsapp   » wa.me/+254716637803
+# System Requirement : Debian 9+ / Ubuntu 18.04+ / 20+
+# Developer  : Abdul NorthAfrica
+# GitHub     : https://github.com/AbdulNorthAfrica
+# Telegram   : https://t.me/AbdulNorthAfrica
+# WhatsApp   : wa.me/+0000000000000
+# Description: TCP BBR Installation & System Optimization Script
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 red='\e[1;31m'
@@ -13,30 +14,23 @@ purple='\e[0;35m'
 orange='\e[0;33m'
 NC='\e[0m'
 clear
-#if [[ -e /usr/local/sbin/bbr ]]; then
-     echo ""
-#     echo -e "${green}install BBR${NC}"
-     echo ""
-#	 read -n1 -r -p "Press any key to continue..."
-#	 menu
-#else
 
-echo -e "Installing TCP BBR"
-echo -e "Please Wait BBR Installation Will Starting . . ."
-sleep 5
+echo ""
+echo -e "${green}Installing TCP BBR and optimizing system parameters...${NC}"
+sleep 3
 clear
 
 touch /usr/local/sbin/bbr
 
 Add_To_New_Line(){
-	if [ "$(tail -n1 $1 | wc -l)" == "0"  ];then
+	if [ "$(tail -n1 $1 | wc -l)" == "0" ]; then
 		echo "" >> "$1"
 	fi
 	echo "$2" >> "$1"
 }
 
 Check_And_Add_Line(){
-	if [ -z "$(cat "$1" | grep "$2")" ];then
+	if [ -z "$(grep "$2" "$1")" ]; then
 		Add_To_New_Line "$1" "$2"
 	fi
 }
@@ -44,51 +38,55 @@ Check_And_Add_Line(){
 Install_BBR(){
 echo -e "\e[32;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 echo -e "\e[32;1mInstalling TCP BBR...\e[0m"
-if [ -n "$(lsmod | grep bbr)" ];then
-echo -e "\e[0;32mSuccesfully Installed TCP BBR.\e[0m"
-echo -e "\e[32;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
-return 1
+if [ -n "$(lsmod | grep bbr)" ]; then
+	echo -e "\e[0;32mTCP BBR is already installed.\e[0m"
+	echo -e "\e[32;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
+	return 1
 fi
-echo -e "\e[0;32mStarting To Install BBR...\e[0m"
+echo -e "\e[0;32mStarting TCP BBR installation...\e[0m"
 modprobe tcp_bbr
 Add_To_New_Line "/etc/modules-load.d/modules.conf" "tcp_bbr"
 Add_To_New_Line "/etc/sysctl.conf" "net.core.default_qdisc = fq"
 Add_To_New_Line "/etc/sysctl.conf" "net.ipv4.tcp_congestion_control = bbr"
 sysctl -p
-if [ -n "$(sysctl net.ipv4.tcp_available_congestion_control | grep bbr)" ] && [ -n "$(sysctl net.ipv4.tcp_congestion_control | grep bbr)" ] && [ -n "$(lsmod | grep "tcp_bbr")" ];then
-	echo -e "\e[0;32mTCP BBR Install Success!\e[0m"
+if [ -n "$(sysctl net.ipv4.tcp_available_congestion_control | grep bbr)" ] && [ -n "$(sysctl net.ipv4.tcp_congestion_control | grep bbr)" ] && [ -n "$(lsmod | grep "tcp_bbr")" ]; then
+	echo -e "\e[0;32mTCP BBR installation successful!\e[0m"
 else
-	echo -e "\e[1;31mFailed To Install BBR!\e[0m"
+	echo -e "\e[1;31mFailed to install TCP BBR.\e[0m"
 fi
 echo -e "\e[32;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 }
 
 Optimize_Parameters(){
 echo -e "\e[32;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
-echo -e "\e[32;1mOptimize Parameters...\e[0m"
+echo -e "\e[32;1mOptimizing system parameters...\e[0m"
 modprobe ip_conntrack
+
+# File limits
 Check_And_Add_Line "/etc/security/limits.conf" "* soft nofile 65535"
 Check_And_Add_Line "/etc/security/limits.conf" "* hard nofile 65535"
 Check_And_Add_Line "/etc/security/limits.conf" "root soft nofile 51200"
 Check_And_Add_Line "/etc/security/limits.conf" "root hard nofile 51200"
-################################
-##############################
+
+# IPv4 forwarding
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.all.route_localnet=1"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.ip_forward = 1"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.all.forwarding = 1"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.default.forwarding = 1"
-################################
+
+# IPv6 forwarding
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.conf.all.forwarding = 1"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.conf.default.forwarding = 1"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.conf.lo.forwarding = 1"
-################################
+
+# IPv6 settings
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.conf.all.disable_ipv6 = 0"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.conf.default.disable_ipv6 = 0"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.conf.lo.disable_ipv6 = 0"
-################################
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.conf.all.accept_ra = 2"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.conf.default.accept_ra = 2"
-################################
+
+# Kernel and TCP tuning
 Check_And_Add_Line "/etc/sysctl.conf" "net.core.netdev_budget = 50000"
 Check_And_Add_Line "/etc/sysctl.conf" "net.core.netdev_budget_usecs = 5000"
 Check_And_Add_Line "/etc/sysctl.conf" "#fs.file-max = 51200"
@@ -98,81 +96,27 @@ Check_And_Add_Line "/etc/sysctl.conf" "net.core.rmem_default = 67108864"
 Check_And_Add_Line "/etc/sysctl.conf" "net.core.wmem_default = 67108864"
 Check_And_Add_Line "/etc/sysctl.conf" "net.core.optmem_max = 65536"
 Check_And_Add_Line "/etc/sysctl.conf" "net.core.somaxconn = 10000"
-################################
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.icmp_echo_ignore_all = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.icmp_echo_ignore_broadcasts = 1"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.icmp_ignore_bogus_error_responses = 1"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.all.accept_redirects = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.default.accept_redirects = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.all.secure_redirects = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.default.secure_redirects = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.all.send_redirects = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.default.send_redirects = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.default.rp_filter = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.all.rp_filter = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_keepalive_time = 1200"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_keepalive_intvl = 15"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_keepalive_probes = 5"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_synack_retries = 2"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_syncookies = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_rfc1337 = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_timestamps = 1"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_tw_reuse = 1"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_fin_timeout = 15"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.ip_local_port_range = 1024 65535"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_max_tw_buckets = 2000000"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_fastopen = 3"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_rmem = 4096 87380 67108864"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_wmem = 4096 65536 67108864"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.udp_rmem_min = 8192"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.udp_wmem_min = 8192"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_mtu_probing = 0"
-##############################
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.all.arp_ignore = 2"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.default.arp_ignore = 2"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.all.arp_announce = 2"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.conf.default.arp_announce = 2"
-##############################
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_autocorking = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_slow_start_after_idle = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_max_syn_backlog = 30000"
-Check_And_Add_Line "/etc/sysctl.conf" "net.core.default_qdisc = fq"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_congestion_control = bbr"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_notsent_lowat = 16384"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_no_metrics_save = 1"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_ecn = 2"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_ecn_fallback = 1"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_frto = 0"
-##############################
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.conf.all.accept_redirects = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.conf.default.accept_redirects = 0"
-Check_And_Add_Line "/etc/sysctl.conf" "vm.swappiness = 1"
-Check_And_Add_Line "/etc/sysctl.conf" "vm.overcommit_memory = 1"
-Check_And_Add_Line "/etc/sysctl.conf" "#vm.nr_hugepages=1280"
-Check_And_Add_Line "/etc/sysctl.conf" "kernel.pid_max=64000"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.neigh.default.gc_thresh3=8192"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.neigh.default.gc_thresh2=4096"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.neigh.default.gc_thresh1=2048"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.neigh.default.gc_thresh3=8192"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.neigh.default.gc_thresh2=4096"
-Check_And_Add_Line "/etc/sysctl.conf" "net.ipv6.neigh.default.gc_thresh1=2048"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_max_syn_backlog = 262144"
 Check_And_Add_Line "/etc/sysctl.conf" "net.netfilter.nf_conntrack_max = 262144"
 Check_And_Add_Line "/etc/sysctl.conf" "net.nf_conntrack_max = 262144"
+Check_And_Add_Line "/etc/sysctl.conf" "vm.swappiness = 1"
+Check_And_Add_Line "/etc/sysctl.conf" "vm.overcommit_memory = 1"
+Check_And_Add_Line "/etc/sysctl.conf" "kernel.pid_max=64000"
 
-##############################
-##############################
+# Systemd limits
 Check_And_Add_Line "/etc/systemd/system.conf" "DefaultTimeoutStopSec=30s"
 Check_And_Add_Line "/etc/systemd/system.conf" "DefaultLimitCORE=infinity"
 Check_And_Add_Line "/etc/systemd/system.conf" "DefaultLimitNOFILE=65535"
-echo -e "\e[0;32mSuccesfully Optimize Parameters.\e[0m"
+
+echo -e "\e[0;32mSystem parameters optimized successfully.\e[0m"
 echo -e "\e[32;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 }
+
 Install_BBR
 Optimize_Parameters
+
 rm -f /root/bbr.sh >/dev/null 2>&1
 echo -e '\e[32;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m'
-echo -e '\e[0;32m       Installation Success!       \e[0m'
+echo -e '\e[0;32m       Installation Completed!       \e[0m'
 echo -e '\e[32;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m'
 sleep 1
-#fi
