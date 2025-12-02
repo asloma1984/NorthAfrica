b65b0a20b579743cf1b150f9b71b76b49a367848
abdul <59056864+asloma1984@users.noreply.github.com>
2025-12-02 23:20:59 +0000

    Update limit.sh

--- files/limit.sh
+++ files/limit.sh
@@ -7,19 +7,27 @@
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # North Africa Script
 
-REPO="https://raw.githubusercontent.com/johntezali56/PREMIER-/main/"
-wget -q -O /etc/systemd/system/limitvmess.service "${REPO}files/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitvless.service "${REPO}files/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limittrojan.service "${REPO}files/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}files/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vmess "${REPO}limit/vmess" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vless "${REPO}limit/vless" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.trojan "${REPO}limit/trojan" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.shadowsocks "${REPO}limit/shadowsocks" >/dev/null 2>&1
+REPO="https://raw.githubusercontent.com/asloma1984/NorthAfrica/main/"
+
+# Download systemd services
+wget -q -O /etc/systemd/system/limitvmess.service "${REPO}files/limitvmess.service"
+wget -q -O /etc/systemd/system/limitvless.service "${REPO}files/limitvless.service"
+wget -q -O /etc/systemd/system/limittrojan.service "${REPO}files/limittrojan.service"
+wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}files/limitshadowsocks.service"
+
+# Download limit scripts
+wget -q -O /etc/xray/limit.vmess "${REPO}limit/vmess"
+wget -q -O /etc/xray/limit.vless "${REPO}limit/vless"
+wget -q -O /etc/xray/limit.trojan "${REPO}limit/trojan"
+wget -q -O /etc/xray/limit.shadowsocks "${REPO}limit/shadowsocks"
+
+# Permissions
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
 chmod +x /etc/xray/limit.trojan
 chmod +x /etc/xray/limit.shadowsocks
+
+# Enable services
 systemctl daemon-reload
 systemctl enable --now limitvmess
 systemctl enable --now limitvless
